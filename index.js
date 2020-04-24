6475fbc401c4dd59f1224c6e159a2c6621d68735
Ian Crowther <ian@iancrowther.co.uk>
2020-04-24 23:24:40 +0100

    final 2

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ const main = async () => {
     const workingDir = core.getInput('workingDir')
 
     const filePath = path.join(workingDir, filename)
+
     const data = await fs.readFile(filePath, 'utf8')
     const updated = data.replace(/blank_host/g, url.value)
-
+    console.log(updated)
+    
     await fs.writeFile(filePath, updated, 'utf8')
   } catch (error) {
     core.setFailed(error.message);
