6ae42e6cecaeb23e6b5fd31ce8323b0bbe603230
Ian Crowther <ian@iancrowther.co.uk>
2020-04-24 17:09:29 +0100

    blah1

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs')
 
-try {
-  const url = core.getInput('url');
-  console.log(`${url}!`);
-  
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  
-  fs.readFile('yo.json', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+const readFile = async () => {
+  try {
+    const data = await fs.readFile('yo.json')
+    return data;
+  } catch (error) {
+    throw error
+  }
 }
+
+const main = () => {
+  try {
+    const url = core.getInput('url');
+    console.log(`${url}!`);
+    
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    
+    console.log(`yo: ${readFile()}`)
+  
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
