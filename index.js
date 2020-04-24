20ae366d3a99f0ba457950b2d736c3a05146f6da
Ian Crowther <ian@iancrowther.co.uk>
2020-04-24 17:27:31 +0100

    blah

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { promises: fs } = require('fs');
 const main = async () => {
   try {
     const url = core.getInput('url')
-    const data = await fs.readFile('bo.json', 'utf8')
+    const data = await fs.readFile('postman_environment.json', 'utf8')
     await fs.writeFile('bo.json', data.replace(/blank_host/g, url), 'utf8')
   } catch (error) {
     core.setFailed(error.message);
