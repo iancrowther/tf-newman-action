9cf63ea95fa37f34a4e4981e6acb6d440a1041ec
Ian Crowther <ian@iancrowther.co.uk>
2020-04-24 16:14:57 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const url = core.getInput('url');
+  console.log(`${url}!`);
+  
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
