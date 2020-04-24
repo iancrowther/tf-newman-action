b31ba6a636b8d80296a464a4612e42ae3ec73830
Ian Crowther <ian@iancrowther.co.uk>
2020-04-24 19:05:48 +0100

    5

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const main = async () => {
   console.log('main')
   try {
     const filename = core.getInput('filename') || 'example.json'
+    console.log(filename)
+    
     const url = core.getInput('url') || 'example.com'
     console.log(url)
+    console.log(env)
     
     const data = await fs.readFile(filename, 'utf8')
     const updated = data.replace(/blank_host/g, url)
