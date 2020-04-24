f63c79fd1cbb3cc712f6feb1676879d60882f783
Ian Crowther <ian@iancrowther.co.uk>
2020-04-24 20:01:06 +0100

    e

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ const main = async () => {
     const filename = core.getInput('filename') || 'example.json'
     console.log(filename)
     
-    // const url = core.getInput('url') || 'example.com'
-    // console.log(url)
+    const url = core.getInput('url') || 'example.com'
+    console.log(url)
 
-    const url1 = core.getInput('url1') || 'example.com'
-    console.log(url1)
+    // const url1 = core.getInput('url1') || 'example.com'
+    // console.log(url1)
 
-    const url2 = core.getInput('url2') || 'example.com'
-    console.log(url2)
+    // const url2 = core.getInput('url2') || 'example.com'
+    // console.log(url2)
     
-    // const url3 = core.getInput('url3') || 'example.com'
-    // console.log(url3)
+    // // const url3 = core.getInput('url3') || 'example.com'
+    // // console.log(url3)
     
-    console.log(JSON.stringify(url2))
+    // console.log(JSON.stringify(url2))
     
-    console.log(INPUT_URL)
-    const urlEnv = INPUT_URL
-    console.log(urlEnv.url)
+    // console.log(INPUT_URL)
+    // const urlEnv = INPUT_URL
+    // console.log(urlEnv.url)
     
     const data = await fs.readFile(filename, 'utf8')
     const updated = data.replace(/blank_host/g, url)
